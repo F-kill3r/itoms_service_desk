017efee78ec596366f79a2e7379fc29eb970d478
dduddo <ch8930@naver.com>
2025-05-19 15:50:54 +0900

    init: service desk server 프로젝트 초기 세팅
    - 기본 세팅 진행(web mvc, jpa, lombok, mysql)
    - 스웨거 기본 설정까지만 진행

---
+++ src/main/java/com/capston_design/fkiller/itoms/service_desk/ServiceDeskApplication.java
@@ -0,0 +1,12 @@
+package com.capston_design.fkiller.itoms.service_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capston_design/fkiller/itoms/service_desk/ServiceDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston_design.fkiller.itoms.service_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
